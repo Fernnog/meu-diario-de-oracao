d84d4175e124e962b8366a328d2ce8fbcd9f9f14
Fernnog <fernandocnogueira@gmail.com>
2025-07-10 10:45:16 -0400

    Add files via upload

---
+++ ui.js
@@ -0,0 +1,7 @@
+case 'archive': {
+    showConfirmationModal("Arquivar este alvo?", async () => {
+        // ...lógica para arquivar...
+        // (Coloque a lógica que estava dentro do `if` aqui)
+    });
+    break;
+}
