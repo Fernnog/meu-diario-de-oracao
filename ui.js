230f2de7d7e78a67e33781a7bb77a454b12fff90
Fernnog <fernandocnogueira@gmail.com>
2025-07-10 12:01:12 -0400

    Delete ui.js

--- ui.js
+++
@@ -1,7 +0,0 @@
-case 'archive': {
-    showConfirmationModal("Arquivar este alvo?", async () => {
-        // ...lógica para arquivar...
-        // (Coloque a lógica que estava dentro do `if` aqui)
-    });
-    break;
-}
